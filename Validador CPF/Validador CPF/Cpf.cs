d3663fad075bb6a3bc603bcb207e173c7e959b28
VictorSantos09 <110687512+VictorSantos09@users.noreply.github.com>
2022-09-17 21:33:48 -0300

    Add files via upload

---
+++ Validador CPF/Validador CPF/Cpf.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validador_CPF
+{
+    public class Cpf
+    {
+        public string CpfNumero { get; set; }
+        public bool LetterChecker()
+        {
+            string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for (int i = 0; i < alfabeto.Length; i++)
+            {
+                char letraAlfabeto = alfabeto[i];
+                for (int j = 0; j < CpfNumero.Length; j++)
+                {
+                    if (CpfNumero.ToUpper()[j] == letraAlfabeto)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public bool CpfLenghtChecker()
+        {
+            if (CpfNumero.Length == 11)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Validador CPF/Validador CPF/Program.cs
@@ -0,0 +1,34 @@
+﻿using Validador_CPF;
+
+bool userUsing = true;
+Cpf cpf = new Cpf();
+
+bool looping = true;
+while (looping)
+{
+    Console.WriteLine("Informe um CPF: ");
+    cpf.CpfNumero = Console.ReadLine();
+    bool lenghtOk = cpf.CpfLenghtChecker();
+    if (lenghtOk == true)
+    {
+        Console.WriteLine("Tamanho válido\n");
+    }
+    if(lenghtOk == false)
+    {
+        Console.WriteLine("Tamanho inválido\n");
+    }
+    bool caracteresValidos = cpf.LetterChecker();
+    if (caracteresValidos == true)
+    {
+        Console.WriteLine("CPF não contém Letras\n");
+    }
+    else
+    {
+        Console.WriteLine("CPF contém Letras, tente novamente\n");
+    }
+    if (caracteresValidos && lenghtOk == true)
+    {
+        Console.WriteLine(Convert.ToUInt64(cpf.CpfNumero).ToString(@"CPF: 000\.000\.000\-00"));
+        looping = false;
+    }
+}
